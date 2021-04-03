9cbed906c1c0aa4f384b056d21400bc5fd9a1836
Vadim <573626+kosyak@users.noreply.github.com>
2021-04-03 23:07:25 +0300

    Reverted shaders to version 100

--- src/painter.rs
+++ src/painter.rs
@@ -34,20 +34,21 @@ struct UserTexture {
 }
 
 const VS_SRC: &str = r#"
-    #version 150
+    #version 100
+    precision mediump float;
     uniform vec2 u_screen_size;
-    in vec2 a_pos;
-    in vec4 a_srgba; // 0-255 sRGB
-    in vec2 a_tc;
-    out vec4 v_rgba;
-    out vec2 v_tc;
+    attribute vec2 a_pos;
+    attribute vec4 a_srgba; // 0-255 sRGB
+    attribute vec2 a_tc;
+    varying vec4 v_rgba;
+    varying vec2 v_tc;
 
     // 0-1 linear  from  0-255 sRGB
     vec3 linear_from_srgb(vec3 srgb) {
         bvec3 cutoff = lessThan(srgb, vec3(10.31475));
         vec3 lower = srgb / vec3(3294.6);
         vec3 higher = pow((srgb + vec3(14.025)) / vec3(269.025), vec3(2.4));
-        return mix(higher, lower, cutoff);
+        return mix(higher, lower, vec3(cutoff));
     }
 
     vec4 linear_from_srgba(vec4 srgba) {
@@ -66,11 +67,11 @@ const VS_SRC: &str = r#"
 "#;
 
 const FS_SRC: &str = r#"
-    #version 150
+    #version 100
+    precision mediump float;
     uniform sampler2D u_sampler;
-    in vec4 v_rgba;
-    in vec2 v_tc;
-    out vec4 f_color;
+    varying vec4 v_rgba;
+    varying vec2 v_tc;
 
     // 0-255 sRGB  from  0-1 linear
     vec3 srgb_from_linear(vec3 rgb) {
@@ -84,6 +85,7 @@ const FS_SRC: &str = r#"
         return vec4(srgb_from_linear(rgba.rgb), 255.0 * rgba.a);
     }
     
+    // 0-1 linear from 0-255 sRGB
     vec3 linear_from_srgb(vec3 srgb) {
         bvec3 cutoff = lessThan(srgb, vec3(10.31475));
         vec3 lower = srgb / vec3(3294.6);
@@ -96,9 +98,9 @@ const FS_SRC: &str = r#"
     }
 
     void main() {
-        // Need to convert from SRGBA to linear.
-        vec4 texture_rgba = linear_from_srgba(texture(u_sampler, v_tc) * 255.0);
-        f_color = v_rgba * texture_rgba;
+        vec4 texture_rgba = linear_from_srgba(texture2D(u_sampler, v_tc) * 255.0);
+        gl_FragColor = srgba_from_linear(v_rgba * texture_rgba) / 255.0;
+        //gl_FragColor = v_rgba * texture2D(u_sampler, v_tc) * 255.0;    
     }
 "#;
 
